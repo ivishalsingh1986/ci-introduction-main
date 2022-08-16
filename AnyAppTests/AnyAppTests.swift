0bc5eb7b6e760315f027b33527558f076743ec6b
Vishal Singh <38453675+ivishalsingh1986@users.noreply.github.com>
2022-08-16 22:47:17 +0800

    Initial commit

---
+++ AnyAppTests/AnyAppTests.swift
@@ -0,0 +1,19 @@
+//
+//  AnyAppTests.swift
+//  AnyAppTests
+//
+//  Created by tdolenko on 06.07.22.
+//
+
+import XCTest
+@testable import AnyApp
+
+class AnyAppTests: XCTestCase {
+
+    func testExample() {
+        XCTAssertEqual(4, 4)
+    }
+
+   
+
+}
